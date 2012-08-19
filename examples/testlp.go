991e425b2adf945378a64029e27e0d292d2b3e6e
Harri Rautila <harri.rautila@gmail.com>
2012-08-19 20:16:18 +0300

    Cosmetics.

--- examples/testlp.go
+++ examples/testlp.go
@@ -17,18 +17,16 @@ func main() {
 	G := matrix.FloatMatrixStacked(gdata, matrix.ColumnOrder)
 	h := matrix.FloatVector([]float64{3.0, 3.0, 0.0, 0.0})
 
-	fmt.Printf("G=\n%v\n", G.ToString("%.2f"))
-	fmt.Printf("h=\n%v\n", h.ToString("%.2f"))
-
 	var solopts cvx.SolverOptions
 	solopts.MaxIter = 30
 	solopts.ShowProgress = true
 	sol, err := cvx.Lp(c, G, h, nil, nil, &solopts, nil, nil)
-	fmt.Printf("status: %v\n", err)
 	if sol != nil && sol.Status == cvx.Optimal {
 		fmt.Printf("x=\n%v\n", sol.Result.At("x")[0].ToString("%.9f"))
 		fmt.Printf("s=\n%v\n", sol.Result.At("s")[0].ToString("%.9f"))
 		fmt.Printf("z=\n%v\n", sol.Result.At("z")[0].ToString("%.9f"))
+	} else {
+		fmt.Printf("status: %v\n", err)
 	}
 }
 
