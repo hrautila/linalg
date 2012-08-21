dabc617ca4f5eda5b652f5d7d40a0c7967e7547d
Harri Rautila <harri.rautila@gmail.com>
2012-08-22 01:54:46 +0300

    New function Syevx

---
+++ lapack/syevx.go
@@ -0,0 +1,196 @@
+
+// Copyright (c) Harri Rautila, 2012
+
+// This file is part of go.opt/linalg package. It is free software, distributed
+// under the terms of GNU Lesser General Public License Version 3, or any later
+// version. See the COPYING tile included in this archive.
+
+package lapack
+
+import (
+	"github.com/hrautila/go.opt/linalg"
+	"github.com/hrautila/go.opt/matrix"
+	"errors"
+	"fmt"
+)
+
+
+/*
+ Computes selected eigenvalues and eigenvectors of a real symmetric
+ matrix (expert driver).
+
+ m = Syevx(A, W, jobz=PJobNo, range=PRangeAll, uplo=PLower,
+ vlimit=[]float{0.0,0.0},  ilimit=[]int{1,1}, Z=nil, n=A.Rows(),
+ ldA=max(1,A.Rows()), ldZ=-1, abstol=0.0, offsetA=0, offsetW=0, offsetZ=0)
+
+ PURPOSE
+
+ Computes selected eigenvalues/vectors of a real symmetric n by n
+ matrix A.
+ 
+ If range is PRangeAll, all eigenvalues are computed.
+ If range is PRangeValue, all eigenvalues in the interval (vlimit[0],vlimit[1]] are
+ computed.
+ If range is PRangeInt, all eigenvalues il through iu are computed
+ (sorted in ascending order with 1 <= il <= iu <= n).
+
+ If jobz is PJobNo, only the eigenvalues are returned in W.
+ If jobz is PJobValue, the eigenvectors are also returned in Z.
+
+ On exit, the content of A is destroyed.
+
+ ARGUMENTS
+  A         float matrix
+  W         float matrix of length at least n.  On exit, contains
+            the computed eigenvalues in ascending order.
+  Z         float matrix.  Only required when jobz is PJobValue.  If range
+            is PRangeAll or PRangeValue, Z must have at least n columns.  If
+            range is PRangeInt, Z must have at least iu-il+1 columns.
+            On exit the first m columns of Z contain the computed (normalized) eigenvectors.
+  vlimit    []float64 or nul.  Only required when range is PRangeValue
+  ilimit    []int or nil.  Only required when range is PRangeInt.
+  abstol    double.  Absolute error tolerance for eigenvalues.
+            If nonpositive, the LAPACK default value is used.
+
+ OPTIONS
+  jobz      'N' or 'V'
+  range     'A', 'V' or 'I'
+  uplo      'L' or 'U'
+  n         integer.  If negative, the default value is used.
+  m         the number of eigenvalues computed;
+  ldA       nonnegative integer.  ldA >= max(1,n).  If zero, the
+            default value is used.
+  ldZ       nonnegative integer.  ldZ >= 1 if jobz is PJobNo and
+            ldZ >= max(1,n) if jobz is PJobValue.  The default value
+            is 1 if jobz is PJobNo and max(1,Z.size[0]) if jobz =PJobValue.
+            If zero, the default value is used.
+  offsetA   nonnegative integer
+  offsetW   nonnegative integer
+  offsetZ   nonnegative integer
+*/
+func Syevx(A, W, Z matrix.Matrix, abstol float64, vlimit []float64, ilimit []int, opts ...linalg.Option) error {
+	if ! matrix.EqualTypes(A, W, Z) {
+		return errors.New("Syevx: not same type")
+	}
+	switch A.(type) {
+	case *matrix.FloatMatrix:
+		Am := A.(*matrix.FloatMatrix)
+		Wm := W.(*matrix.FloatMatrix)
+		Zm := Z.(*matrix.FloatMatrix)
+		return SyevrFloat(Am, Wm, Zm, abstol, vlimit, ilimit, opts...)
+	}
+	return errors.New("Syevr: unknown types")
+}
+
+func SyevxFloat(A, W, Z matrix.Matrix, abstol float64, vlimit []float64, ilimit []int, opts ...linalg.Option) error {
+	var vl, vu float64
+	var il, iu int
+
+	pars, err := linalg.GetParameters(opts...)
+	if err != nil {
+		return err
+	}
+	ind := linalg.GetIndexOpts(opts...)
+	if ind.N < 0 {
+		ind.N = A.Rows()
+		if ind.N != A.Cols() {
+			return errors.New("Syevr: A not square")
+		}
+	}
+	// Check indexes
+	if ind.N == 0 {
+		return nil
+	}
+	if ind.LDa == 0 {
+		ind.LDa = max(1, A.Rows())
+	}
+	if ind.LDa < max(1, A.Rows()) {
+		return errors.New("Syevr: lda")
+	}
+	if pars.Range == linalg.PRangeValue {
+		if vlimit == nil {
+			return errors.New("Syevx: vlimit is nil")
+		}
+		vl = vlimit[0]
+		vu = vlimit[1]
+		if vl >= vu {
+			return errors.New("Syevx: must be: vl < vu")
+		}
+	} else if pars.Range == linalg.PRangeInt {
+		if ilimit == nil {
+			return errors.New("Syevx: ilimit is nil")
+		}
+		il = ilimit[0]
+		iu = ilimit[1]
+		if il < 1 || il > iu || iu > ind.N {
+			return errors.New("Syevx: must be:1 <= il <= iu <= N")
+		}
+	}
+	if pars.Jobz == linalg.PJobValue {
+		if Z == nil {
+			return errors.New("Syevx: Z is nil")
+		}
+		if ind.LDz == 0 {
+			ind.LDz = max(1, Z.Rows())
+		}
+		if ind.LDz < max(1, ind.N) {
+			return errors.New("Syevx: ldz")
+		}
+	} else {
+		if ind.LDz == 0 {
+			ind.LDz = 1
+		}
+		if ind.LDz < 1 {
+			return errors.New("Syevx: ldz")
+		}
+	}
+	if ind.OffsetA < 0 {
+		return errors.New("Syevx: OffsetA")
+	}
+	sizeA := A.NumElements()
+	if sizeA < ind.OffsetA + (ind.N-1)*ind.LDa + ind.N {
+		return errors.New("Syevx: sizeA")		
+	}
+	if ind.OffsetW < 0 {
+		return errors.New("Syevx: OffsetW")
+	}
+	sizeW := W.NumElements()
+	if sizeW < ind.OffsetW + ind.N {
+		return errors.New("Syevx: sizeW")		
+	}
+	if pars.Jobz == linalg.PJobValue {
+		if ind.OffsetZ < 0 {
+			return errors.New("Syevx: OffsetW")
+		}
+		minZ := ind.OffsetZ + (ind.N-1)*ind.LDz + ind.N
+		if pars.Range == linalg.PRangeInt {
+			minZ = ind.OffsetZ + (iu-il)*ind.LDz + ind.N
+		}
+		if Z.NumElements() < minZ {
+			return errors.New("Syevx: sizeZ")
+		}
+	}
+			
+	Aa := A.FloatArray()
+	Wa := W.FloatArray()
+	var Za []float64
+	if pars.Jobz == linalg.PJobValue {
+		Za = Z.FloatArray()
+	} else {
+		Za = nil
+	}
+	jobz := linalg.ParamString(pars.Jobz)
+	rnge := linalg.ParamString(pars.Range)
+	uplo := linalg.ParamString(pars.Uplo)
+	
+	info := dsyevx(jobz, rnge, uplo, ind.N, Aa[ind.OffsetA:], ind.LDa,
+		vl, vu, il, iu, ind.M, Wa[ind.OffsetW:], Za, ind.LDz)
+	if info != 0 {
+		return errors.New(fmt.Sprintf("Syevx: call failed %d", info))
+	}
+	return nil
+}
+
+// Local Variables:
+// tab-width: 4
+// End:
