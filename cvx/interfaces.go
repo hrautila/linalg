63d2f2ce94ae2b93f41eb93b17b67d2ded802e19
Harri Rautila <harri.rautila@gmail.com>
2012-09-23 23:54:14 +0300

    Collected public interface types and internal implementations of them here.

--- cvx/interfaces.go
+++ cvx/interfaces.go
@@ -11,6 +11,8 @@ package cvx
 
 import (
 	"github.com/hrautila/go.opt/matrix"
+	"github.com/hrautila/go.opt/linalg/blas"
+	"github.com/hrautila/go.opt/cvx/sets"
 	la "github.com/hrautila/go.opt/linalg"
 )
 
@@ -60,7 +62,10 @@ type MatrixVarA interface {
 type MatrixP interface {
 	Pf(u, v *matrix.FloatMatrix, alpha, beta float64) error
 }
-
+//
+// MatrixVarP provides interface for extended customization with non-matrix type
+// primal and dual variables.
+//
 type MatrixVarP interface {
 	Pf(u, v MatrixVariable, alpha, beta float64) error
 }
@@ -106,14 +111,60 @@ type ConvexProg interface {
 	F2(x, z *matrix.FloatMatrix)(f, Df, H *matrix.FloatMatrix, err error)
 }
 
+// ConvexVarProg provides interface for extended customization with non-matrix type
+// primal and dual variables.
+//
+type ConvexVarProg interface {
+	// Returns (mnl, x0) where mln number of nonlinear inequality constraints
+	// and x0 is a point in the domain of f.
+	F0() (mnl int, x0 MatrixVariable, err error)
 
+	// Returns a tuple (f, Df) where f contains f(x) and Df is instance of
+	// MatrixVarDf interface.
+	F1(x MatrixVariable)(f MatrixVariable, Df MatrixVarDf, err error)
+	
+	// F(x, z) with z a positive matrix variable. Return a tuple
+	// (f, Df, H), where f, Df as above. H is an instance of MatrixVarH interface.
+	F2(x, z MatrixVariable)(f MatrixVariable, Df MatrixVarDf, H MatrixVarH, err error)
+}
+
+// Public interface to provide custom Df matrix-vector products
+//
+// The call Df(u, v, alpha, beta, trans) should evaluate the matrix-vectors products
+//
+//   v := alpha * Df(x) * u + beta * v if trans is linalg.OptNoTrans
+//   v := alpha * Df(x)' * u + beta * v if trans is linalg.OptTrans.
+//
+type MatrixDf interface {
+	Df(u, v *matrix.FloatMatrix, alpha, beta float64, trans la.Option) error
+}
+
+// MatrixVarDf provides interface for extended customization with non-matrix type
+// primal and dual variables.
+//
 type MatrixVarDf interface {
 	Df(u, v MatrixVariable, alpha, beta float64, trans la.Option) error
 }
 
+
+// Public interface to provide custom H matrix-vector products
+//
+// The call H(u, v, alpha, beta) should evaluate the matrix-vectors product
+//
+//    v := alpha * H * u + beta * v.
+//
+type MatrixH interface {
+	Hf(u, v *matrix.FloatMatrix, alpha, beta float64) error
+}
+
+// MatrixVarH provides interface for extended customization with non-matrix type
+// primal and dual variables.
+//
 type MatrixVarH interface {
 	Hf(u, v MatrixVariable, alpha, beta float64) error
 }
+
+
 
 // Empty interface for 
 type Variable interface {
@@ -127,7 +178,7 @@ type Variable interface {
 // If u is an object of type implementing MatrixVariable interface, then
 type MatrixVariable interface {
 	// Provide internal matrix value
-	AsMatrix() *matrix.FloatMatrix
+	Matrix() *matrix.FloatMatrix
 	// Create a new copy 
 	Copy() MatrixVariable
 	// Computes v := alpha*u + v for a scalar alpha and vectors u and v.
@@ -140,6 +191,159 @@ type MatrixVariable interface {
 	Verify(vals ...interface{}) float64
 	// Convert to string for printing
 	String() string
+}
+
+// Copies x to y.
+func mCopy(x, y MatrixVariable) {
+	y.Scal(0.0)
+	x.Axpy(y, 1.0)
+}
+
+
+// Implementations of interfaces for standard matrix valued parameters
+
+// Impelements MatrixVariable interface standard matrix valued variable
+type matrixVar struct {
+	val *matrix.FloatMatrix
+}
+
+// Internal matrix
+func (u *matrixVar) Matrix() *matrix.FloatMatrix {
+	return u.val
+}
+
+// New copy
+func (u *matrixVar) Copy() MatrixVariable {
+	return &matrixVar{u.val.Copy()}
+}
+
+// Inner product
+func (u *matrixVar) Dot(v MatrixVariable) float64 {
+	if y, ok := v.(*matrixVar); ok {
+		return blas.DotFloat(u.val, y.val)
+	}
+	return 0.0
+}
+
+func (u *matrixVar) Axpy(v MatrixVariable, alpha float64) {
+	if y, ok := v.(*matrixVar); ok {
+		blas.AxpyFloat(u.val, y.val, alpha)
+	}
+	return
+}
+
+func (u *matrixVar) Scal(alpha float64) {
+	blas.ScalFloat(u.val, alpha)
+}
+
+func (u *matrixVar) String() string {
+	return u.val.ToString("%.7f")
+}
+
+func (u *matrixVar) Verify(vals ...interface{}) float64 {
+	diff := 0.0
+	for _, v := range vals {
+		if vm, ok := v.(*matrix.FloatMatrix); ok {
+			diff += blas.Nrm2Float(matrix.Minus(vm, u.val))
+		}
+	}
+	return diff
+}
+
+// Implements MatrixVarG interface for matrix valued G
+type matrixVarG struct {
+	G *matrix.FloatMatrix
+	dims *sets.DimensionSet
+}
+
+func (g *matrixVarG) Gf(u, v MatrixVariable, alpha, beta float64, trans la.Option) (err error) {
+	err = sgemv(g.G, u.Matrix(), v.Matrix(), alpha, beta, g.dims, trans)
+	return
+}
+
+// Implements MatrixVarG interface for MatrixG interface
+type matrixIfG struct {
+	G MatrixG
+}
+
+func (g *matrixIfG) Gf(u, v MatrixVariable, alpha, beta float64, trans la.Option) (err error) {
+	err = g.G.Gf(u.Matrix(), v.Matrix(), alpha, beta, trans)
+	return
+}
+
+
+// Implements MatrixVarA interface for matrix valued A
+type matrixVarA struct {
+	A *matrix.FloatMatrix
+}
+
+func (a *matrixVarA) Af(u, v MatrixVariable, alpha, beta float64, trans la.Option) (err error) {
+	err = blas.GemvFloat(a.A, u.Matrix(), v.Matrix(), alpha, beta, trans) 
+	return
+}
+
+// Implements MatrixVarA interface for MatrixA interface
+type matrixIfA struct {
+	A MatrixA
+}
+
+func (a *matrixIfA) Af(u, v MatrixVariable, alpha, beta float64, trans la.Option) (err error) {
+	err = a.A.Af(u.Matrix(), v.Matrix(), alpha, beta, trans) 
+	return
+}
+
+// Implements ConvexVarProg interface for standard matrices
+type convexVarProg struct {
+	cp ConvexProg
+}
+
+func (p *convexVarProg) F0() (mnl int, x0 MatrixVariable, err error) {
+	x0 = nil
+	var mx0 *matrix.FloatMatrix
+	mnl, mx0, err = p.cp.F0()
+	if err != nil { return }
+	x0 = &matrixVar{mx0}
+	return
+}
+
+func (p *convexVarProg) F1(x MatrixVariable) (f MatrixVariable, Df MatrixVarDf, err error) {
+	f = nil; Df = nil
+	var f0, Df0 *matrix.FloatMatrix
+	f0, Df0, err = p.cp.F1(x.Matrix())
+	if err != nil { return }
+	f = &matrixVar{f0}
+	Df = &matrixVarDf{Df0}
+	return
+}
+
+func (p *convexVarProg) F2(x, z MatrixVariable) (f MatrixVariable, Df MatrixVarDf, H MatrixVarH, err error) {
+	f = nil; Df = nil; H = nil
+	var f0, Df0, H0 *matrix.FloatMatrix
+	f0, Df0, H0, err = p.cp.F2(x.Matrix(), z.Matrix())
+	if err != nil { return }
+	f = &matrixVar{f0}
+	Df = &matrixVarDf{Df0}
+	H = &matrixVarH{H0}
+	return
+}	
+
+
+// Implements MatrixVarDf interface for standard matrix
+type matrixVarDf struct {
+	df *matrix.FloatMatrix
+}
+
+func (d *matrixVarDf) Df(u, v MatrixVariable, alpha, beta float64, trans la.Option) error {
+	return blas.GemvFloat(d.df, u.Matrix(), v.Matrix(), alpha, beta, trans)
+}
+
+// Implements MatrixVarH interface for standard matrix 
+type matrixVarH struct {
+	h *matrix.FloatMatrix
+}
+
+func (hf *matrixVarH) Hf(u, v MatrixVariable, alpha, beta float64) error {
+	return blas.SymvFloat(hf.h, u.Matrix(), v.Matrix(), alpha, beta)
 }
 
 
