0a9b8916e044073849739b26ec5d70fae396bffd
Harri Rautila <harri.rautila@gmail.com>
2012-09-29 17:14:58 +0300

    Analytic centering test.

--- lapack/lapack_test.go
+++ lapack/lapack_test.go
@@ -4,23 +4,17 @@ package lapack
 import (
 	"github.com/hrautila/matrix"
 	"testing"
-	"fmt"
 )
 
-// simple matrix implementaion for testing
-
+// simple matrix implementation for testing
 
 func TestDGetrf(t *testing.T) {
 	ipiv := make([]int32, 3)
 	A := matrix.FloatNew(3, 2, []float64{1, 2, 3, 4, 5, 6})
-	fmt.Printf("pre A:\n%s\n", A)
+	t.Logf("pre A:\n%s\n", A)
 	err := Getrf(A, ipiv)
-	fmt.Printf("err=%s, ipiv=%s\n", err, ipiv)
-	fmt.Printf("post A:\n%s\n", A)
-}
-
-func TestFoo(t *testing.T) {
-	fmt.Printf("OK\n")
+	t.Logf("err=%v, ipiv=%v\n", err, ipiv)
+	t.Logf("post A:\n%s\n", A)
 }
 
 
